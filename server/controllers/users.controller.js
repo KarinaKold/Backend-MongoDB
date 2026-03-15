a697eabfdce47663241436ef586b630a9ec40167
Karina Koldaeva <karina.mila@yandex.ru>
2026-03-15 22:39:00 +0300

    fix: delete comments

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -8,18 +8,6 @@ async function addUser(patient) {
 async function getUsers(search = "", limit = 10, page = 1) {
   const users = await User.find();
   return users;
-  // const [users, count] = await Promise.all([
-  //   User.find({ name: { $regex: search, $options: "i" } })
-  //     .limit(limit)
-  //     .skip((page - 1) * limit)
-  //     .sort({ createdAt: -1 }),
-  //   User.countDocuments({ name: { $regex: search, $options: "i" } }),
-  // ]);
-
-  // return {
-  //   users,
-  //   lastPage: Math.ceil(count / limit),
-  // };
 }
 
 module.exports = {
